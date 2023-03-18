4c47a677d2000ef4b35788c340418362baf44afe
junrdev <juniorprogrammer09@gmail.com>
2023-03-18 23:13:21 +0300

    added getEmail from jwt method

---
+++ security2/src/main/java/com/sec/config/JwtService.java
@@ -0,0 +1,16 @@
+package com.sec.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+	public String extractUsername(String jwt) {
+		// TODO Auto-generated method stub
+		return null;
+		
+	}
+
+	
+
+}
