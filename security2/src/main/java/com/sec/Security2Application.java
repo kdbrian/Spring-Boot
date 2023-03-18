8d2c61868ee191368922dd79803d834f503cf650
junrdev <juniorprogrammer09@gmail.com>
2023-03-18 22:52:13 +0300

    some dev stuff

--- security2/src/main/java/com/sec/Security2Application.java
+++ security2/src/main/java/com/sec/Security2Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Security2Application {
+public class Security2Application{
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Security2Application.class, args);
 	}
 
+	
 }
