e78bb6be62bb2b51e894888897c7520540920883
junrdev <juniorprogrammer09@gmail.com>
2023-03-18 12:29:02 +0300

    spring security

---
+++ security2/src/main/java/com/sec/Security2Application.java
@@ -0,0 +1,13 @@
+package com.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security2Application.class, args);
+	}
+
+}
