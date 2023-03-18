8fb9980c9234f00ba9862a36ed05c3fa3ad80674
junrdev <juniorprogrammer09@gmail.com>
2023-03-18 23:12:08 +0300

    implemented filter method

--- security2/src/main/java/com/sec/config/JwtAuthenticationFilter.java
+++ security2/src/main/java/com/sec/config/JwtAuthenticationFilter.java
@@ -1,5 +1,49 @@
 package com.sec.config;
 
-public class JwtAuthenticationFilter {
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+	@Autowired
+	private final JwtService jwtservice;
+	
+	
+	@Override
+	protected void doFilterInternal(
+			@NonNull HttpServletRequest request, 
+			@NonNull HttpServletResponse response, 
+			@NonNull FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		final String authHeader = request.getHeader("Authorization");//header that contains bearer token
+		
+		final String jwt;
+		final String usrname;
+		
+		//check if there is a bearer token or authorization the Bearer token doesnt exist
+		if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		jwt= authHeader.substring(7);
+		jwtservice.extractUsername(jwt);
+		
+		
+	}
 
 }
