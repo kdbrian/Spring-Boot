e9afc660fc122758d67d07a4769d4727153bdcb6
junrdev <juniorprogrammer09@gmail.com>
2023-03-29 19:44:40 +0300

    some tests

--- projects/Data-JPA/src/test/java/com/data/DataJpaApplicationTests.java
+++ projects/Data-JPA/src/test/java/com/data/DataJpaApplicationTests.java
@@ -1,13 +1,32 @@
 package com.data;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.data.entity.Student;
+import com.data.repository.StudentRepository;
 
 @SpringBootTest
 class DataJpaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Test
+	public void saveStudent() {
+		
+		Student student = Student.builder()
+				.firstName("Brian")
+				.lastName("Kidiga")
+				.email("brian@dev.io")
+				.build();
+		
+		studentRepository.save(student);
 	}
 
 }
