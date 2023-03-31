4017c5a61d8dec913838026c59ee2400ef8e83e2
junrdev <juniorprogrammer09@gmail.com>
2023-03-31 16:56:38 +0300

    added new query methods

--- learning/Data-JPA/src/main/java/com/data/repository/StudentRepository.java
+++ learning/Data-JPA/src/main/java/com/data/repository/StudentRepository.java
@@ -1,6 +1,11 @@
 package com.data.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.data.entity.Student;
@@ -8,6 +13,7 @@ import com.data.entity.Student;
 import java.util.List;
 
 @Repository
+@EnableJpaRepositories
 public interface StudentRepository extends JpaRepository<Student, Long>{
 
     //adding own methods
@@ -32,4 +38,37 @@ public interface StudentRepository extends JpaRepository<Student, Long>{
     List<Student> findByFirstNameAndLastName(String fname, String lname);
     List<Student> findByFirstNameOrLastName(String fname, String lname);
 
+//    alternatively the method that require prior
+//    implementation can be annotated with @Query
+    @Query("select s from Student s where s.email = ?1")//JPQL QUERY
+    //passing class& attribute names not table & column names
+    //the ?# represents the parameter number(?1 -> parameter 1)
+    public Student getStudentByEmailAdress(String email);
+
+    @Query("SELECT s.firstName from Student s where s.email = ?1")
+    //whenever jus a specific field is needed
+    public String getStudentFirstNameByEmailAdress(String email);
+
+    @Query(
+            value = "select * from tbl_student s where s.email = ?1",
+            nativeQuery = true
+    )//native queries just like normal sql
+    public Student getStudentByEmailAdressNative(String email);
+
+
+    @Query(
+            value = "select * from tbl_student s where s.email = :email ",
+            nativeQuery = true
+    )//native queries with named parameters
+    public Student getStudentByEmailAdressNativeNamedParam( @Param("email") String email);
+
+
+    @Modifying//make this method modify persisted data
+    @Transactional//must tbe there
+    // alows execution of certain transactions (modifying, delete ) and are committed at once
+    @Query(
+            value = "update tbl_student set first_name = :firstname where email = :email",
+            nativeQuery = true
+    )
+    int updateStudentNameByEmail(@Param("firstname") String firstname, @Param("email") String email);
 }
