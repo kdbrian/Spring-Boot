74385380ab36c20f2e1daff863b4e4a26ab5d9bf
junrdev <juniorprogrammer09@gmail.com>
2023-03-31 16:59:16 +0300

    the coursematerial entity

---
+++ learning/Data-JPA/src/main/java/com/data/repository/CourseMaterialRepository.java
@@ -0,0 +1,9 @@
+package com.data.repository;
+
+import com.data.entity.CourseMaterial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseMaterialRepository extends JpaRepository<CourseMaterial, Long> {
+}
