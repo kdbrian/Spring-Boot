58dbfa7c982e320cbe13720ba549e1bb03d11b28
junrdev <juniorprogrammer09@gmail.com>
2023-03-31 17:01:49 +0300

    the course material repository

---
+++ learning/Data-JPA/src/test/java/com/data/CourseMaterialRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.data;
+
+import com.data.entity.Course;
+import com.data.entity.CourseMaterial;
+import com.data.repository.CourseMaterialRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CourseMaterialRepositoryTest {
+
+    @Autowired
+    private CourseMaterialRepository courseMaterialRepository;
+
+    @Test
+    public void testSaveCOurseMaterial(){
+        Course course = Course.builder()
+                .courseTitle("Computer Science")
+                .credit(90000)
+                .build();//trying to add this to a course material fails since it need a reference from the course table
+        //for that case cascading is used
+        CourseMaterial courseMaterial = CourseMaterial.builder()
+                .course(course)
+                .url("Http://127.0.0.1")
+                .build();
+
+        System.out.println(courseMaterialRepository.save(courseMaterial));
+    }
+}
