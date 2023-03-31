27a4d350e058f4f8e159f3569cafd33204580411
junrdev <juniorprogrammer09@gmail.com>
2023-03-31 16:58:01 +0300

    the course entity

---
+++ learning/Data-JPA/src/main/java/com/data/entity/Course.java
@@ -0,0 +1,31 @@
+package com.data.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Course {
+
+    @Id
+    @SequenceGenerator(
+            name = "course_seq",
+            sequenceName = "course_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_seq"
+    )
+    private Long courseId;
+    private String courseTitle;
+    private Integer credit;
+
+}
