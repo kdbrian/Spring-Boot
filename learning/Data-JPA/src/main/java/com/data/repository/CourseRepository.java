5bf0719bcc28fc0667bc71d897dbe7471b390814
junrdev <juniorprogrammer09@gmail.com>
2023-03-31 17:00:45 +0300

    the course repository

---
+++ learning/Data-JPA/src/main/java/com/data/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.data.repository;
+
+import com.data.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
