123d0e53eceed018e8aab3bc3d1af9817578002e
junrdev <juniorprogrammer09@gmail.com>
2023-04-02 22:36:27 +0300

    added pagination method

--- learning/Data-JPA/src/main/java/com/data/repository/CourseRepository.java
+++ learning/Data-JPA/src/main/java/com/data/repository/CourseRepository.java
@@ -1,9 +1,16 @@
 package com.data.repository;
 
 import com.data.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    Page<Course> findByCourseTitleContaining(String title, Pageable request);
 }
