9be0bc963d0ffd640cddfed2c6539540bb1c0518
junrdev <juniorprogrammer09@gmail.com>
2023-04-02 21:59:50 +0300

    teacher repository

---
+++ learning/Data-JPA/src/main/java/com/data/repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package com.data.repository;
+
+import com.data.entity.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
