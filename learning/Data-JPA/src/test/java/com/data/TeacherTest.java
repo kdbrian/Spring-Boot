33df24de4743f5cf332e43d20c24702b0debfb4d
junrdev <juniorprogrammer09@gmail.com>
2023-04-02 21:59:11 +0300

    teacher entity tests

---
+++ learning/Data-JPA/src/test/java/com/data/TeacherTest.java
@@ -0,0 +1,59 @@
+package com.data;
+
+import com.data.entity.Course;
+import com.data.entity.CourseMaterial;
+import com.data.entity.Teacher;
+import com.data.repository.TeacherRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class TeacherTest {
+
+    private final TeacherRepository teacherRepository;
+
+    @Autowired
+    public TeacherTest(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
+
+    @Test
+    public void SaveTeacher(){
+        Course course = Course.builder()
+                .courseTitle("Hide and seek")
+                .credit(5)
+                .build();
+        Course course2 = Course.builder()
+                .courseTitle("Hide and seek")
+                .credit(5)
+                .build();
+
+        Teacher teacher = Teacher.builder()
+                .firstName("Brian")
+                .lastName("Kidiga")
+//                .courseList(List.of(course, course2))
+                .build();
+
+        System.out.println(teacherRepository.save(teacher));
+    }
+
+    @Test
+    public void saveTeacherCourse(){
+
+        Teacher teacher = Teacher.builder()
+                .firstName("Yellow")
+                .lastName("Brown")
+                .build();
+
+        Course course = Course.builder()
+                .courseTitle("Well..")
+                .credit(7)
+                .teacher(teacher)
+                .build();
+
+        System.out.println(teacherRepository.save(teacher));
+    }
+}
