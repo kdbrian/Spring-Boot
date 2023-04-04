9533a34d8755b647301b92d7e7ad7b380f6c95b2
junrdev <juniorprogrammer09@gmail.com>
2023-04-04 08:12:11 +0300

    added annotations

--- learning/relations/src/main/java/com/relations/_01/Adress.java
+++ learning/relations/src/main/java/com/relations/_01/Adress.java
@@ -1,8 +1,20 @@
 package com.relations._01;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+/***
+ * A Embeddable class works to modularize the characteristics whilist not introducing new
+ * entity classes
+ */
 public class Adress {
 
     private String location;
