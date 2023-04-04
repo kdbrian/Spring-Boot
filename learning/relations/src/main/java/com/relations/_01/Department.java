d564af15f8499d275c0481b20155c81209c9461f
junrdev <juniorprogrammer09@gmail.com>
2023-04-04 08:02:59 +0300

    adress embedable

--- learning/relations/src/main/java/com/relations/_01/Department.java
+++ learning/relations/src/main/java/com/relations/_01/Department.java
@@ -1,4 +1,24 @@
 package com.relations._01;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Department {
+
+    @Id
+    @SequenceGenerator(name = "dept_seq", sequenceName = "dept_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dept_seq")
+    private Long departmentId;
+
+    private String departmentName;
+    private String departmentHead;
+    private String departmentLocation;
 }
