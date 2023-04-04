c5549c0cac1abc8785585276cb2c459cc2778232
junrdev <juniorprogrammer09@gmail.com>
2023-04-04 07:57:20 +0300

    added new field and defined attributes

--- learning/relations/src/main/java/com/relations/_01/Employee.java
+++ learning/relations/src/main/java/com/relations/_01/Employee.java
@@ -1,6 +1,6 @@
 package com.relations._01;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,13 +13,29 @@ import java.util.Date;
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(
+        uniqueConstraints = @UniqueConstraint(
+                name = "uniq_email",
+                columnNames = "email"
+        )
+)
 public class Employee {
 
+    @Column( name = "employee_id")
+    @Id
+    @SequenceGenerator( name = "empl_seq", sequenceName = "empl_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "empl_seq")
     private Long id;
+
+    @Column( name = "first_name")
     private String fname;
+
+    @Column( name = "last_name")
     private String lname;
 
     private Date dateJoined;
+
+    private String email;
 
     private Boolean isActive;
 }
