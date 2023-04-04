7dfdbdc651726511c55aed673726ddb4593b8daf
junrdev <juniorprogrammer09@gmail.com>
2023-04-04 08:10:20 +0300

    creating employee wihth adress details

--- learning/relations/src/test/java/com/relations/_01/EmployeeRepoTest.java
+++ learning/relations/src/test/java/com/relations/_01/EmployeeRepoTest.java
@@ -33,6 +33,24 @@ public class EmployeeRepoTest {
     }
 
     @Test
+    public void testInsertEmployeeAdress(){
+        Adress adress = Adress.builder()
+                .location("Karatina")
+                .zipCode(1957l)
+                .build();
+        Employee employee = Employee.builder()
+                .adress(adress)
+                .email("brian2@dev.io")
+                .isActive(true)
+                .fname("brian")
+                .lname("the II")
+                .dateJoined(new Date(System.currentTimeMillis()))
+                .build();
+
+        employeeRepository.save(employee);
+    }
+
+    @Test
     public void testFetchEmployees(){
         List<Employee> employees = employeeRepository.findAll();
         System.out.println("employees = " + employees);
