b55025564d646583c213d77c211225114db8c9de
junrdev <juniorprogrammer09@gmail.com>
2023-04-04 08:33:36 +0300

    tested employee with department info'

--- learning/relations/src/test/java/com/relations/_01/EmployeeRepoTest.java
+++ learning/relations/src/test/java/com/relations/_01/EmployeeRepoTest.java
@@ -12,11 +12,13 @@ import java.util.List;
 public class EmployeeRepoTest {
 
     private final EmployeeRepository employeeRepository;
+    private final DepartmentRepository departmentRepository;
 
 
     @Autowired
-    public EmployeeRepoTest(EmployeeRepository employeeRepository) {
+    public EmployeeRepoTest(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository) {
         this.employeeRepository = employeeRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     @Test
@@ -50,6 +52,47 @@ public class EmployeeRepoTest {
         employeeRepository.save(employee);
     }
 
+    @Test
+    public void testInsertEmployeeWithDepartment(){
+        //cascading must be allowed to all to enable creation of both items
+
+        Department department = Department.builder()
+                .departmentName("No bitches 2")
+                .departmentLocation("Crib Int")
+                .departmentHead("Brian Kidiga")
+                .build();
+
+        Employee employee = Employee.builder()
+                .department(department)
+                .dateJoined(new Date(System.currentTimeMillis()))
+                .fname("j")
+                .lname("cole")
+                .email("j@cole.io")
+                .isActive(true)
+                .build();
+
+        employeeRepository.save(employee);
+    }
+
+    @Test
+    public void testInsertEmployeeWithDepartmentExisting(){
+        //assuming we already know the id of the dept to which the employee shud be registered
+        Department department = departmentRepository.findById(1l).get();
+        Adress adress = Adress.builder()
+                .location("Tano nane")
+                .zipCode(58l)
+                .build();
+        Employee employee = Employee.builder()
+                .fname("yellow")
+                .lname("boy")
+                .email("yellow@boy.io")
+                .isActive(true)
+                .dateJoined(new Date(System.currentTimeMillis()))
+                .adress(adress)
+                .build();
+
+        employeeRepository.save(employee);
+    }
     @Test
     public void testFetchEmployees(){
         List<Employee> employees = employeeRepository.findAll();
