6de140535f8018fcbef8546251af862b8feb9b0e
junrdev <juniorprogrammer09@gmail.com>
2023-04-04 07:47:47 +0300

    initial employee class

--- learning/relations/src/main/java/com/relations/_01/Employee.java
+++ learning/relations/src/main/java/com/relations/_01/Employee.java
@@ -1,4 +1,25 @@
 package com.relations._01;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Employee {
+
+    private Long id;
+    private String fname;
+    private String lname;
+
+    private Date dateJoined;
+
+    private Boolean isActive;
 }
