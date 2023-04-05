0f83a1ae6eea94b82edbe54413614f9019aa314d
junrdev <juniorprogrammer09@gmail.com>
2023-04-05 21:28:49 +0300

    tested insert student with school functionality

--- learning/relations/src/test/java/com/relations/_03/SchoolRepoTest.java
+++ learning/relations/src/test/java/com/relations/_03/SchoolRepoTest.java
@@ -1,9 +1,37 @@
 package com.relations._03;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class SchoolRepoTest {
 
+
+    private final SchoolRepository schoolRepository;
+
+    @Autowired
+    public SchoolRepoTest(SchoolRepository schoolRepository) {
+        this.schoolRepository = schoolRepository;
+    }
+
+    @Test
+    void contextLoads(){}
+
+    @Test
+    public void testSaveSchool(){
+        School school = School.builder()
+                .schoolName("Pure and applied sciences")
+                .build();
+        schoolRepository.save(school);
+    }
+
+    @Test
+    public void testFetchSchools(){
+        List<School> schools = schoolRepository.findAll();
+        System.out.println("schools = " + schools);
+    }
 
 }
