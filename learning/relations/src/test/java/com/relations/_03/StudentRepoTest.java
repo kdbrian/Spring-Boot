010546a822a8d8ce60073115317b70d42b75e6df
junrdev <juniorprogrammer09@gmail.com>
2023-04-05 08:52:44 +0300

    student repo tests initial

---
+++ learning/relations/src/test/java/com/relations/_03/StudentRepoTest.java
@@ -0,0 +1,21 @@
+package com.relations._03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StudentRepoTest {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentRepoTest(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Test
+    public void saveDefaultStudent(){
+
+    }
+}
