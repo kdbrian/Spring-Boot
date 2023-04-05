926b055993aec96ba7efd32269222eb5ab4dfe21
junrdev <juniorprogrammer09@gmail.com>
2023-04-05 08:57:22 +0300

    added an PK in the school class

--- learning/relations/src/main/java/com/relations/_03/School.java
+++ learning/relations/src/main/java/com/relations/_03/School.java
@@ -6,7 +6,7 @@ package com.relations._03;
 * this brings about a many-to-one relationship
 * */
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,6 +19,9 @@ import lombok.NoArgsConstructor;
 @Builder
 public class School {
 
+    @Id
+    @SequenceGenerator(name = "school_seq", sequenceName = "school_seq", allocationSize = 1)
+    @GeneratedValue( strategy = GenerationType.SEQUENCE,generator = "school_seq")
     private Long schoolId;
 
     private String schoolName;
